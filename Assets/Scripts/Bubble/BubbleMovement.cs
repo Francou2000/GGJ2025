8a5634aabe85be65f895d1530b8c5bc60742a62d
SimM20 <83238073+SimM20@users.noreply.github.com>
2025-01-24 14:58:03 -0300

    Updated Menu and Bubble movement

--- Assets/Scripts/Bubble/BubbleMovement.cs
+++ Assets/Scripts/Bubble/BubbleMovement.cs
@@ -2,27 +2,36 @@ using UnityEngine;
 
 public class BubbleMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private float moveForce = 10f;
+    [SerializeField] private float maxSpeed = 5f;
+    [SerializeField] private float moveDelay = 0.5f;
     private Rigidbody2D rb;
-    private Vector2 movementInput;
+    private float lastMoveTime;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
+    void Start() 
+    { 
+        rb = GetComponent<Rigidbody2D>(); 
+        lastMoveTime = -moveDelay; 
     }
     
-    private void FixedUpdate()
-    {
-        Move();
-
-        rb.linearVelocity = movementInput * moveSpeed;
-    }
+    private void FixedUpdate() { Move(); }
 
     private void Move()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        if (Time.time - lastMoveTime >= moveDelay)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
 
-        movementInput = new Vector2(horizontal, vertical);
+            Vector2 direction = new Vector2(horizontal, vertical).normalized;
+
+            rb.AddForce(direction * moveForce, ForceMode2D.Impulse);
+
+            if (rb.linearVelocity.magnitude > maxSpeed)
+            {
+                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+            }
+            lastMoveTime = Time.time;
+        }
     }
 }
