9ff4597a9717dc286518a449d39fec70958c32e5
SimM20 <83238073+SimM20@users.noreply.github.com>
2025-01-25 20:54:54 -0300

    Added resume game button on pause

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     //FALTA TODA LA PARTE DE CONTROL DE SLIDERS DE VOLUMEN QUE VA POR ACA
+    public void TurnOffPause() { UIManager.Instance.PauseMenu(); }
     public void LoadLevelByName(string name) { LoadScenesUtils.LoadSceneByName(name); }
 }
